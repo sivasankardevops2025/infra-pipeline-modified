8181640bdc770221a6b50790026fef5198f899c6
sivasankardevops2025 <sivasankardevops2025@gmail.com>
2026-01-09 12:17:50 +0530

    increasing the EC2

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -2,7 +2,7 @@ module "VPC" {
   source                = "../../Modules/VPC"
   vpc_cidr_block        = "10.0.0.0/16"
   subnet_cidr           =  "10.0.1.0/24"
-  subnet_availability_zone =   "${var.aws_region}a" 
+  subnet_availability_zone =   "${var.aws_region}a"  
   env                   = "dev"
   
 }
@@ -12,6 +12,6 @@ module "ec2" {
   ami_id        = "ami-01dc51e87421923b6" 
   instance_type = "t3.micro"
   subnet_id     =   module.VPC.subnet_id
-  ec2_count     = 1
+  ec2_count     = 2
    env          = "dev"
 }
